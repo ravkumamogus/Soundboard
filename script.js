643c33125aa09d8997e8ebd02b1e90c14570cfce
Re ve <108709054+ravkumamogus@users.noreply.github.com>
2023-03-20 03:42:13 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,140 @@
+function playHi(){
+  var music = new Audio('sounds/hi.mp3');
+  music.play();
+}
+function playBoing(){
+  var music = new Audio('sounds/jump.mp3');
+  music.play();
+}
+function playMusic(){
+  var music = new Audio('sounds/emotional damage.mp3');
+  music.play();
+}
+function playKeyboard(){
+  var music = new Audio('sounds/Key.mp3');
+  music.play();
+}
+function playNo(){
+  var music = new Audio('sounds/NO.mp3');
+  music.play();
+}
+function playCricket(){
+	  var music = new Audio('sounds/cricket.mp3');
+  music.play();
+}
+function playNice(){
+	  var music = new Audio('sounds/nice.mp3');
+  music.play();
+}
+function playBye(){
+	  var music = new Audio('sounds/bye-have-a-great-time.mp3');
+  music.play();
+}
+function playHelp(){
+	  var music = new Audio('sounds/help.mp3');
+  music.play();
+}
+function playSurprise(){
+	  var music = new Audio('sounds/surprise.mp3');
+  music.play();
+}	
+function playSus(){
+	  var music = new Audio('sounds/sus.mp3');
+  music.play();
+}	
+function playVine(){
+	  var music = new Audio('sounds/vine-boom.mp3');
+  music.play();
+}
+function playError(){
+	  var music = new Audio('sounds/erro.mp3');
+  music.play();
+}
+function playDing(){
+	  var music = new Audio('sounds/ding.mp3');
+  music.play();
+}
+function playCheer(){
+	  var music = new Audio('sounds/cheering.mp3');
+  music.play();
+}
+function playYehe(){
+	  var music = new Audio('sounds/yehe boi.mp3');
+  music.play();
+}
+function playHuh(){
+	  var music = new Audio('sounds/huh.mp3');
+  music.play();
+}
+function playFBI(){
+	  var music = new Audio('sounds/fbi.mp3');
+  music.play();
+}
+function playBong(){
+	  var music = new Audio('sounds/bong.mp3');
+  music.play();
+}
+function playDog(){
+	  var music = new Audio('sounds/dog.mp3');
+  music.play();
+}
+function playWhopper(){
+	  var music = new Audio('sounds/whopper-ad.mp3');
+  music.play();
+}
+function playBowser(){
+	  var music = new Audio('sounds/bowser.mp3');
+  music.play();
+}
+function playQuandale(){
+	  var music = new Audio('sounds/quandale.mp3');
+  music.play();
+}
+function playFrench(){
+	  var music = new Audio('sounds/french-meme-song.mp3');
+  music.play();
+}
+function playAlia(){
+	  var music = new Audio('sounds/alia.mp3');
+  music.play();
+}
+function playMarioOver(){
+	  var music = new Audio('sounds/mario-game.mp3');
+  music.play();
+}
+function playSicko(){
+	  var music = new Audio('sounds/sicko.mp3');
+  music.play();
+}
+function playPipe(){
+	  var music = new Audio('sounds/pipe.mp3');
+  music.play();
+}
+function playAugh(){
+	  var music = new Audio('sounds/aaughhh.mp3');
+  music.play();
+}
+function playFortnite(){
+	  var music = new Audio('sounds/fortnite-kill.mp3');
+  music.play();
+}
+function playMysterious(){
+	  var music = new Audio('sounds/xfilestheme.mp3');
+  music.play();
+}
+function playBing(){
+	  var music = new Audio('sounds/bingchilling.mp3');
+  music.play();
+}
+function playObama(){
+	  var music = new Audio('sounds/obama-beatbox.mp3');
+  music.play();
+}
+function playNoGod(){
+	  var music = new Audio('sounds/no-god.mp3');
+  music.play();
+}
+function playNpesta(){
+	  var music = new Audio('sounds/alff.mp3');
+  music.play();
+}
